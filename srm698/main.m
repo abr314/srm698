ab5444bb3ea7a52145157223c04f1dea9db067c8
abr314 <abr314@icloud.com>
2016-10-16 14:47:09 -0500

    Initial Commit

---
+++ srm698/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  srm698
+//
+//  Created by Abraham Brovold on 10/16/16.
+//  Copyright (c) 2016 Abraham Brovold. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
